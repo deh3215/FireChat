36d78fd8bf0ecee8b39fd31b3114448448cbdb6a
deh3215 <3215009@gmail.com>
2017-12-19 14:58:39 +0800

    Fix ListView update item Bug.

--- app/src/main/java/com/example/deh3215/firechat/MainActivity.java
+++ app/src/main/java/com/example/deh3215/firechat/MainActivity.java
@@ -22,9 +22,6 @@ import java.util.HashMap;
 public class MainActivity extends AppCompatActivity {
 
     User user;
-
-    FirebaseDatabase database;
-    DatabaseReference myRef;
     DatabaseReference reference_contacts;
     EditText et1, et2;
     ListView lv;
@@ -33,38 +30,16 @@ public class MainActivity extends AppCompatActivity {
     private final String NAME="name";
     private final String AGE="age";
     static long count=0;
-    //ArrayAdapter<String> adapter;
     SimpleAdapter adapter;
-    //private ArrayList<User> list = new ArrayList<>();
-    ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String,String>>();
+    ArrayList<HashMap<String,String>> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        et1 = (EditText)findViewById(R.id.editText1);
-        et2 = (EditText)findViewById(R.id.editText2);
-        lv = (ListView)findViewById(R.id.listView);
-        //===========write data==============
-//        myRef.setValue("Hello, World!");
-//        writeNewUser(1234, "Jimmy", 46);
-//        String[] id={"1", "2"};
-//        User[] users = new User[id.length];
-//        for(int i=0;i<users.length;i++)
-//            users[i] = new User();
-//
-//        users[0].setName("Jimmy");
-//        users[0].setAge(46);
-//
-//        users[1].setName("Tommy");
-//        users[1].setAge(23);
-//
-//        for(int i=0;i<users.length;i++)
-//            writeNewUser((i+1), users[i].getName(), users[i].getAge());
-//        adapter = new ArrayAdapter<User>(this,
-//                        android.R.layout.simple_list_item_1,
-//                        android.R.id.text1);
-
+        et1 = findViewById(R.id.editText1);
+        et2 = findViewById(R.id.editText2);
+        lv = findViewById(R.id.listView);
         adapter = new SimpleAdapter(
                 this,
                 list,
@@ -76,34 +51,34 @@ public class MainActivity extends AppCompatActivity {
         reference_contacts.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                list.clear();
                 for (DataSnapshot ds : dataSnapshot.getChildren() ){
-                    HashMap<String,String> item = new HashMap<String,String>();
+                    HashMap<String,String> item = new HashMap<>();
                     item.put( NAME, ds.child(NAME).getValue().toString());
                     item.put( AGE,ds.child(AGE).getValue().toString());
-                    //adapter.add(ds.child("name").getValue().toString());
                     user = ds.getValue(User.class);
+                    adapter.notifyDataSetChanged();
                     list.add(item);
-                    lv.setAdapter(adapter);
                 }
                 count = dataSnapshot.getChildrenCount();
-                Log.d("Count", "count="+count+" getKey="+dataSnapshot.getKey());
+                //Log.d("Count", "count="+count+" getKey="+dataSnapshot.getKey());
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
             }
         });
+        lv.setAdapter(adapter);
     }
     //一次寫進一個Object物件資料
     private void writeNewUser(long userId, String name, int age) {
-        User user = new User(name, age);
+        user = new User(name, age);
         reference_contacts.child(""+userId).setValue(user);
     }
 
-    private void removeUser(long userId, String name, int age)   {
-         //User user = new User(name, age);
-         //reference_contacts.child(""+userId).remove();
-    }
+//    private void removeUser(long userId, String name, int age)   {
+//         reference_contacts.child(""+userId).removeValue();
+//    }
 
     public void onClick(View v)   {
         switch(v.getId()) {
@@ -112,6 +87,8 @@ public class MainActivity extends AppCompatActivity {
                 String age = et2.getText().toString();
                 if (!name.equals("") && !age.equals(""))
                     writeNewUser(count+1, name, Integer.valueOf(age));
+                adapter.notifyDataSetChanged();
+
                 et1.setText("");
                 et2.setText("");
                 break;
